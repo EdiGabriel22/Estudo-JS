7f8175f18428ed41cf891b0c33c7adee6bff57b2
EdiGabriel22 <edigabrielcontato@hotmail.com>
2023-02-02 22:38:52 -0300

    Finalizando lógica e iniciando styles

--- Alura/aluramidi/main.js
+++ Alura/aluramidi/main.js
@@ -1,5 +1,28 @@
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
-document.querySelectorAll('.tecla').onclick = tocaSomPom;
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTecla[contador];
+    const instrumento = tecla.classList[0];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function(evento) {
+        if (evento.code === 'Space') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function() {
+        tecla.classList.add('ativa')
+    }
+}
