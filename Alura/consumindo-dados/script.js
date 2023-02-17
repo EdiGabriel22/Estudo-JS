2ba881aa7697648cf9c2ee099778ff40c85373d5
Edi Gabriel <edigabrielcontato@hotmail.com>
2023-02-17 10:41:24 -0300

    finalizando consumo e tratamento de dados

--- Alura/consumindo-dados/script.js
+++ Alura/consumindo-dados/script.js
@@ -1,9 +1,27 @@
-async function buscaEndereco() {
-    var consultCEP = await fetch('https://viacep.com.br/ws/01001000/json/')
-    var consultCEPConvertida = await consultCEP.json()
-    console.log(consultCEPConvertida)
+async function buscaEndereco(cep) {
+    let mensagemErro = document.getElementById('erro')
+    mensagemErro.innerHTML = ""
+    try {
+        let consultCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        let consultCEPConvertida = await consultCEP.json()
+        if (consultCEPConvertida.erro) {
+            throw Error('CEP não existe!')
+        }
+        let cidade = document.getElementById('cidade')
+        let logradouro = document.getElementById('endereco')
+        let estado = document.getElementById('estado')
+
+        cidade.value = consultCEPConvertida.localidade
+        logradouro.value = consultCEPConvertida.logradouro
+        estado.value = consultCEPConvertida.uf
+
+        console.log(consultCEPConvertida)
+        return consultCEPConvertida
+    } catch (erro) {
+        mensagemErro.innerHTML = `<p>CEP inválido. Tente novamente!</p>`
+        console.log(erro)
+    }
 }
 
-                    
-
-buscaEndereco();
+let cep = document.getElementById('cep')
+cep.addEventListener("focusout", () => buscaEndereco(cep.value))
