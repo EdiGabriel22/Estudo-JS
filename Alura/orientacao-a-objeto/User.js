420577929d59bd44adda931d9c546c2e6514ad07
EdiGabriel22 <edigabrielcontato@hotmail.com>
2023-09-22 18:53:26 -0300

    encapsulamento

--- Alura/orientacao-a-objeto/User.js
+++ Alura/orientacao-a-objeto/User.js
@@ -1,12 +1,10 @@
 export default class User {
-
     #nome
     #email
     #nascimento
     #role
     #ativo
 
-    
     constructor(nome, email, nascimento, role, ativo = true) {
         this.#nome = nome
         this.#email = email
@@ -15,18 +13,34 @@ export default class User {
         this.#ativo = ativo
     }
 
-    #montaObjUser() {
-        return ({
-            nome: this.#nome,
-            email: this.#email,
-            nascimento: this.#nascimento,
-            role: this.#role,
-            ativo: this.#ativo
-        })
+    get nome() {
+        return this.#nome
+    }
+
+    get email() {
+        return this.#email
+    }
+
+    get nascimento() {
+        return this.#nascimento
+    }
+
+    get role() {
+        return this.#role
+    }
+
+    get ativo() {
+        return this.#ativo
+    }
+
+    set nome(novoNome) {
+        if (novoNome === "") {
+            throw new Error("O nome não pode ser vazio")
+        }
+        this.#nome = novoNome
     }
 
     exibirInfos() {
-        const objUser = this.#montaObjUser()
-        return `${objUser.nome}, ${objUser.mail}, ${objUser.nascimento}, ${objUser.role}, ${objUser.ativo}`
+        return `${this.nome}, ${this.mail}, ${this.nascimento}, ${this.role}, ${this.ativo}`
     }
 }
--- Alura/orientacao-a-objeto/index.js
+++ Alura/orientacao-a-objeto/index.js
@@ -6,4 +6,7 @@ const novoUser = new User("João", "joao@gmail.com", "01/01/2000")
 console.log(novoUser.exibirInfos())
 
 const novoAdmin = new Admin("Maria", "r@r.com", "01/01/2000")
-console.log(novoAdmin.exibirInfos())
+console.log(novoAdmin.exibirInfos)
+
+const novoDocente = new Docente("José", "g@g.com", "01/01/2000")
+console.log(novoDocente.exibirInfos())
